27c26018fb1f651eb911fde941b850200433c94a
markothedev <markothedevmail@gmail.com>
2022-08-29 01:30:10 +0100

    added List of shop page

---
+++ pages/shops/_index.style.tsx
@@ -0,0 +1,74 @@
+import styled  from 'styled-components';
+
+
+
+export const AllShopsContainer =styled.div`
+
+max-width: 1000px;
+margin: 10px  auto;
+`
+export const ShopContainer =styled.div`
+
+
+@media screen and (min-width: 500px){
+    display: grid;
+    grid-template-columns: repeat(2,1fr);
+    /* border: 1px solid red; */
+    gap: 20px;
+}
+
+@media screen and (min-width: 800px){
+  grid-template-columns: repeat(3,1fr);
+
+}
+
+`
+
+
+
+
+export const ShopIntroCard =styled.div`
+
+border:1px solid #0000001a;
+padding: 1rem;
+border-radius: 10px;
+width: 90%;
+margin: 10px auto;
+display: flex;
+justify-content: space-between;
+align-items: center;
+max-width: 400px;
+
+& >div:nth-child(1){
+  /* width: 70px; */
+width: 80px;
+
+  /* border: 1px solid yellow; */
+
+img{
+  width: 100%;
+  border-radius: 50%;
+  height: 100%;
+}
+}
+& >div:nth-child(2){
+width: 70%;
+/* border: 1px solid red; */
+p{
+  padding: .4rem 0;
+  color: #00000094;
+}
+h3{
+  font-weight: lighter;
+}
+}
+
+
+
+@media screen and (min-width:500px){
+    margin: unset;
+    width: 100%;
+    max-width: unset;
+}
+
+`
---
+++ pages/shops/index.tsx
@@ -0,0 +1,44 @@
+import { NextPage } from 'next';
+import Image from 'next/image';
+import GeneralLayout from '../../layout/GeneralLayout/GeneralLayout';
+import {HiLocationMarker} from 'react-icons/hi'
+import {AllShopsContainer,ShopContainer,
+  ShopIntroCard}  from './_index.style'
+// import DummyImage from '../../assets/shopimage.webp'
+
+const DummyImage= 'https://pickbazar-react.vercel.app/_next/image?url=https%3A%2F%2Fpickbazarlaravel.s3.ap-southeast-1.amazonaws.com%2F882%2FFurniture.png&w=1920&q=75'
+const index:NextPage=()=>{
+
+
+  return (
+    <GeneralLayout>
+
+      <br /><br /><br />
+      <AllShopsContainer>
+        <h2>All Shops</h2>
+        <br />
+        <ShopContainer >
+          {
+            [...new Array(5)].map((i,index)=>(
+              <ShopIntroCard key={index}>
+                <div>
+                  <img src={DummyImage} className='shop_logo'/>
+                </div>
+                <div>
+                  <h3>Furniture Shop</h3>
+                  <p>
+                    <HiLocationMarker/>
+                    4885 Spring street, llinois, Lincoin,62657,USA
+                  </p>
+                </div>
+              </ShopIntroCard>
+            ))
+          }
+        </ShopContainer>
+
+      </AllShopsContainer>
+    </GeneralLayout>
+  )
+}
+
+export default index
